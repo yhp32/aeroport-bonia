8aa26defaa4dc7fb77f6291a2b7f571b44ab6d0c
yhp32 <93236998+yhp32@users.noreply.github.com>
2021-12-08 15:38:52 +0800

    Update Diamond.js

--- src/components/Diamond.js
+++ src/components/Diamond.js
@@ -8,19 +8,7 @@ export default function Model(props) {
   const { nodes } = useGLTF('/textures/diamond.gltf')
   const { movement } = props
 
-  useFrame(({ clock }) => {
-    if (group.current) {
-      group.current.position.y += Math.sin(clock.getElapsedTime()) * (movement ?? 0.003)
-    }
-  })
-
-  return (
-    <group ref={group} {...props} dispose={null}>
-      <mesh geometry={nodes.Hedra002.geometry}>
-        <meshStandardMaterial color="white" />
-      </mesh>
-    </group>
-  )
+  
 }
 
 useGLTF.preload('/diamond_0.gltf')
