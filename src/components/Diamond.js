180931ac78ac518b280de93a58b731dc4a655eab
yhp32 <93236998+yhp32@users.noreply.github.com>
2021-12-08 15:40:24 +0800

    Update Diamond.js

--- src/components/Diamond.js
+++ src/components/Diamond.js
@@ -8,7 +8,17 @@ export default function Model(props) {
   const { nodes } = useGLTF('/textures/diamond.gltf')
   const { movement } = props
 
-  
+  useFrame(({ clock }) => {
+    if (group.current) {
+      group.current.position.y += Math.sin(clock.getElapsedTime()) * (movement ?? 0.003)
+    }
+  })
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+     
+    </group>
+  )
 }
 
 useGLTF.preload('/diamond_0.gltf')
