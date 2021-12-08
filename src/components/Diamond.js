f5e39330e5594b8c1d988ef79581a2889f2724f8
yhp32 <93236998+yhp32@users.noreply.github.com>
2021-12-08 15:31:45 +0800

    Update Diamond.js

--- src/components/Diamond.js
+++ src/components/Diamond.js
@@ -1,18 +1,3 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei/useGLTF'
 import { useFrame } from 'react-three-fiber'
-
-export default function Model(props) {
-  const group = useRef()
-  const { nodes } = useGLTF('/textures/diamond.gltf')
-  const { movement } = props
-
-  useFrame(({ clock }) => {
-    if (group.current) {
-      group.current.position.y += Math.sin(clock.getElapsedTime()) * (movement ?? 0.003)
-    }
-  })
-
-}
-
-useGLTF.preload('/diamond_0.gltf')
